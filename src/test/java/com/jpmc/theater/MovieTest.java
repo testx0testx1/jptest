8795a8d3e99180a0f2e00f3b7722db21afb75bab
testx0testx1 <96598752+testx0testx1@users.noreply.github.com>
2022-09-01 07:33:38 -0400

    add more test

--- src/test/java/com/jpmc/theater/MovieTest.java
+++ src/test/java/com/jpmc/theater/MovieTest.java
@@ -23,4 +23,32 @@ public class MovieTest {
         Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.of(11,43,12)));
         assertEquals(9.375, spiderMan.calculateTicketPrice(showing));
     }
+
+    @Test
+    void specialMovieWith3DollarDiscount() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),12.5, 1);
+        Showing showing = new Showing(spiderMan, 1, LocalDateTime.of(LocalDate.now(), LocalTime.of(10,43,12)));
+        assertEquals(9.5, spiderMan.calculateTicketPrice(showing));
+    }
+
+    @Test
+    void specialMovieWith2DollarDiscount() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),12.5, 0);
+        Showing showing = new Showing(spiderMan, 2, LocalDateTime.of(LocalDate.now(), LocalTime.of(10,43,12)));
+        assertEquals(10.5, spiderMan.calculateTicketPrice(showing));
+    }
+
+    @Test
+    void specialMovieWith1DollarDiscount() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),12.5, 0);
+        Showing showing = new Showing(spiderMan, 7, LocalDateTime.of(LocalDate.now(), LocalTime.of(10,43,12)));
+        assertEquals(11.5, spiderMan.calculateTicketPrice(showing));
+    }
+
+    @Test
+    void specialMovieWithoutDiscount() {
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),12.5, 0);
+        Showing showing = new Showing(spiderMan, 6, LocalDateTime.of(LocalDate.now(), LocalTime.of(10,43,12)));
+        assertEquals(12.5, spiderMan.calculateTicketPrice(showing));
+    }
 }
